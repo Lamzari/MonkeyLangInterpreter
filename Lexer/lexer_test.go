c1fd82c067479b771f8f15da20210a5d0f8c2199
olamzari <oussama.lamzari@gmail.com>
2023-12-09 22:24:07 +0100

    Added lexing for operators: =+-!*/<> and keywords: true, false, if, else, return

--- Lexer/lexer_test.go
+++ Lexer/lexer_test.go
@@ -7,48 +7,22 @@ import (
 )
 
 func TestNextToken(t *testing.T) {
-	input := `=+(){},;`
-
-	tests := []struct {
-		expectedType    token.TokenType
-		expectedLiteral string
-	}{
-		{token.ASSIGN, "="},
-		{token.PLUS, "+"},
-		{token.LPAREN, "("},
-		{token.RPAREN, ")"},
-		{token.LSQUIRLY, "{"},
-		{token.RSQUIRLY, "}"},
-		{token.COMMA, ","},
-		{token.SEMICOLON, ";"},
-		{token.EOF, ""},
-	}
-
-	l := New(input)
-	for i, tt := range tests {
-		tok := l.NextToken()
-
-		if tok.Type != tt.expectedType {
-			t.Fatalf("tests[%d] - tokentype wrong. expected=%q, got=%q", i, tt.expectedType, tok.Type)
-		}
-
-		if tok.Literal != tt.expectedLiteral {
-			t.Fatalf("tests[%d] - tokenliteral wrong. expected=%q, got=%q", i, tt.expectedLiteral, tok.Literal)
-		}
-	}
-
-}
-
-func TestNextToken2(t *testing.T) {
 	input := `let five = 5;
 let ten = 10;
 
 let add = fn(x, y) {
-x + y;
+  x + y;
 };
 
 let result = add(five, ten);
-`
+!-/*5;
+5 < 10 > 5;
+
+if (5 < 10) {
+  return true;
+} else {
+  return false;
+}`
 	tests := []struct {
 		expectedType    token.TokenType
 		expectedLiteral string
@@ -89,6 +63,42 @@ let result = add(five, ten);
 		{token.IDENT, "ten"},
 		{token.RPAREN, ")"},
 		{token.SEMICOLON, ";"},
+
+		{token.BANG, "!"},
+		{token.MINUS, "-"},
+		{token.SLASH, "/"},
+		{token.ASTERISK, "*"},
+		{token.INT, "5"},
+		{token.SEMICOLON, ";"},
+
+		{token.INT, "5"},
+		{token.LT, "<"},
+		{token.INT, "10"},
+		{token.GT, ">"},
+		{token.INT, "5"},
+		{token.SEMICOLON, ";"},
+
+		{token.IF, "if"},
+		{token.LPAREN, "("},
+		{token.INT, "5"},
+		{token.LT, "<"},
+		{token.INT, "10"},
+		{token.RPAREN, ")"},
+		{token.LSQUIRLY, "{"},
+
+		{token.RETURN, "return"},
+		{token.TRUE, "true"},
+		{token.SEMICOLON, ";"},
+
+		{token.RSQUIRLY, "}"},
+		{token.ELSE, "else"},
+		{token.LSQUIRLY, "{"},
+
+		{token.RETURN, "return"},
+		{token.FALSE, "false"},
+		{token.SEMICOLON, ";"},
+
+		{token.RSQUIRLY, "}"},
 		{token.EOF, ""},
 	}
 
